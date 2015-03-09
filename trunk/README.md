d1c0c646793df674debbd3cda1fe592d47ab0643
Patrick Matthews <Patrick Matthews@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-09 21:27:07 +0000

    fixing tags
    git-svn-id: https://plugins.svn.wordpress.org/gotchosen-social-exchange/tags/1.0.9@1109039 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/README.md
@@ -0,0 +1,21 @@
+GotChosen Social Exchange Plugin
+===================
+
+WP plugin to integrate with GotChosen's Social Exchange API.
+
+The plugin provides the following options:
+
+* Feed Key
+  * The Feed Key provided by your GotChosen publisher account to allow access to the Social Exchange API.
+* Webcurtain Settings
+  * Enable/disable the webcurtain overlay for the site.
+  * Enable/disable the compatability option to fix the webcurtain display on some sites.
+* Social Exchange Publishing Options
+  * Ability to set a default for the per post option of publishing to the Social Exchange API.
+  * Make all Social Exchange posts shareable.
+  * Make all Social Exchange posts commentable.
+  * Ability to set what the 'Read More' text will read when a post is published to the Social Exchange.
+* Per post options
+  * When editing/adding posts there is an options box added that allows a user to choose if they want that post published to the Social Exchange or not.
+
+When a post is sent to the Social Exchange API, if any errors occur, it will automatically be re-sent with each post save until it's successful.
---
+++ trunk/includes/templates/admin.php
@@ -0,0 +1,57 @@
+<div class="wrap">
+  <h2 class="gc_title"><img src="<?php echo $this->includes_url ?>/images/gc-thumb.png" width="30px" />GotChosen Integration</h2>
+  <div>
+    <form action="" method="post">
+      <?php wp_nonce_field('got chosen save options'); ?>
+      <h3>GCID</h3>
+      <div class="gc_opts_group">
+          <div class="gc_option">
+              <label for="gcid">Enter GCID provided to your publisher account: </label>
+              <input type="text" value="<?php echo $this->options['gcid'] ?>" name="gcid" id="gcid" />
+          </div>
+      </div>
+      <h3>Feed Key</h3>
+      <div class="gc_opts_group">
+        <div class="gc_option">
+          <label for="feedkey">Enter Feed Key provided to your publisher account: </label>
+          <input type="text" value="<?php echo $this->options['feedkey'] ?>" name="feedkey" id="feedkey" />
+        </div>
+      </div>
+      <h3>WebCurtain Settings</h3>
+      <div class="gc_opts_group">
+        <div class="gc_option">
+          <label for="webcurtain">Enable the WebCurtain: </label>
+          <input type="checkbox" name="webcurtain" id="webcurtain" <?php echo($this -> options['webcurtain']) ? 'checked="checked"' : ''; ?>
+          />
+        </div>
+        <div class="gc_option">
+          <label for="webcurtain_compat">Enable compatability mode: </label>
+          <input type="checkbox" name="webcurtain_compat" id="webcurtain_compat" <?php echo($this -> options['webcurtain_compat']) ? 'checked="checked"' : ''; ?>
+          />
+          <p class="description">
+            Use compatability mode if the WebCurtain is not displaying properly on your site.
+          </p>
+        </div>
+      </div>
+      <h3>Social Exchange Options</h3>
+      <div class="gc_opts_group">
+        <div class="gc_option">
+          <label for="pub_minifeed_default">Default setting for publishing posts to the Social Exchange: </label>
+          <input type="checkbox" name="pub_minifeed_default" id="pub_minifeed_default" <?php echo($this -> options['pub_minifeed_default']) ? 'checked="checked"' : ''; ?>
+          />
+        </div>
+        <div class="gc_option">
+          <label for="shareable">Make Social Exchange posts shareable: </label>
+          <input type="checkbox" name="shareable" id="shareable" <?php echo($this -> options['shareable']) ? 'checked="checked"' : ''; ?>
+          />
+        </div>
+        <div class="gc_option">
+          <label for="commentable">Make Social Exchange posts commentable: </label>
+          <input type="checkbox" name="commentable" id="commentable" <?php echo($this -> options['commentable']) ? 'checked="checked"' : ''; ?>
+          />
+        </div>
+      </div>
+      <input type="submit" class="button button-primary" value="Update Options">
+    </form>
+  </div>
+</div>
